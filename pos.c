57d5d13778e58d48597657d1841056cb27b1721f
jeevaajay <30457764+jeevaajay@users.noreply.github.com>
2017-07-26 11:07:03 +0530

    Create pos.c

---
+++ pos.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter");
+scanf("%d",&n);
+if(n<0)
+{
+printf("negative");
+}
+else if(n>0)
+{
+printf("positive");
+}
+else if(n=0)
+{
+printf("zero");
+}
+}
