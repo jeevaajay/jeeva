5cdac9587499da77d666dce9994a32b1e2ce126b
jeevaajay <30457764+jeevaajay@users.noreply.github.com>
2017-07-29 16:54:05 +0530

    Create power.8.c

---
+++ power.8.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int base,exponent;
+printf("Enter the base number");
+scanf("%d,&base");
+printf("Enter the exponent number");
+scanf("%d,&exponent");
+while(2<3)
+}
